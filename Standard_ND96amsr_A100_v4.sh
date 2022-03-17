f91fd8da4f3fcc34c5ff8e111406c2f8dae3fb46
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-03-17 20:09:18 +0800

    Add files via upload

---
+++ Standard_ND96amsr_A100_v4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+az group create --name Server --location westeurope
+az vm create --resource-group Server --name westeurope --location westeurope --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC6s_v3 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+sleep 3m
+x=1
+while [ $x -le 500 ]
+do
+  echo "Start vps lan $x"
+  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  echo "Run script lan $x"
+  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
+  for vps in   westeurope
+  do
+    if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
+    then
+      echo "$vps creating..."
+	  az vm create --resource-group Server --name $vps --location $vps --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC6s_v3 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+    else
+      echo "$vps was found."
+    fi
+  done  
+  sleep 1m
+  x=$(( $x + 1 ))
+done
