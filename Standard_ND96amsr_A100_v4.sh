2da2c1925d9b3dd9e63fcee88d84834234a9eb3b
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-03-26 22:20:44 +0800

    Update Standard_ND96amsr_A100_v4.sh

--- Standard_ND96amsr_A100_v4.sh
+++ Standard_ND96amsr_A100_v4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-az group create --name Server --location eastus2
-az vm create --resource-group Server --name eastus2 --location westeurope --image Canonical:UbuntuServer:18.04-LTS:latest --size Standard_ND96amsr_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+az group create --name Server --location westeurope
+az vm create --resource-group Server --name westeurope --location westeurope --image Canonical:UbuntuServer:16_04_0-lts-gen2:latest --size Standard_ND96amsr_A100_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
 sleep 3m
 x=1
 while [ $x -le 500 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  az vm start --ids $(az vm list -g Server --query "[].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
   for vps in   westeurope
@@ -14,7 +14,7 @@ do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
       echo "$vps creating..."
-	  az vm create --resource-group Server --name $vps --location $vps --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_ND96amsr_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+	  az vm create --resource-group Server --name $vps --location $vps --image Canonical:UbuntuServer:16_04_0-lts-gen2:latest --size Standard_ND96amsr_A100_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
     else
       echo "$vps was found."
     fi
