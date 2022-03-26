ac7c0972f0754085fd1df7585183de42f4657426
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-03-27 00:59:41 +0800

    Update Standard_ND96amsr_A100_v4.sh

--- Standard_ND96amsr_A100_v4.sh
+++ Standard_ND96amsr_A100_v4.sh
@@ -9,7 +9,7 @@ do
   az vm start --ids $(az vm list -g Server --query "[].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
-  for vps in   westeurope
+  for vps in westeurope
   do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
