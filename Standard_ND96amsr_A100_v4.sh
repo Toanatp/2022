867a2f8ba8ded09caa763af7cb8fb72f89c61aca
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-04-03 01:24:12 +0800

    Update Standard_ND96amsr_A100_v4.sh

--- Standard_ND96amsr_A100_v4.sh
+++ Standard_ND96amsr_A100_v4.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-az group create --name Server --location westeurope
+az group create --name Server --location eastus2
 az vm create --resource-group Server --name westeurope --location westeurope --image Canonical:UbuntuServer:16_04_0-lts-gen2:latest --size Standard_ND96amsr_A100_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+
 sleep 3m
 x=1
 while [ $x -le 500 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[].id" -o tsv) --no-wait
+  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
-  for vps in westeurope
+  for vps in switzerlandnorth southeastasia eastus eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral
   do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
