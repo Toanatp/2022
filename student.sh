ce037561e7eb8c4bab93926c89faaa002a83d25b
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-05-22 12:19:48 +0800

    Update student.sh

--- student.sh
+++ student.sh
@@ -1,32 +1,20 @@
-woker=$(date +'%d%m_')
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ]; then
-	echo "Start setup..."
-	wget http://us.download.nvidia.com/tesla/410.129/nvidia-diag-driver-local-repo-ubuntu1604-410.129_1.0-1_amd64.deb
-	sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604-410.129_1.0-1_amd64.deb
-	sudo apt-key add /var/nvidia-diag-driver-local-repo-410.129/7fa2af80.pub
-	sudo apt-get update
-	sudo apt-get -y install cuda-drivers --allow-unauthenticated
-	sudo apt-get install gcc g++ build-essential libssl-dev automake linux-headers-$(uname -r) git gawk libcurl4-openssl-dev libjansson-dev xorg libc++-dev libgmp-dev python-dev -y
-	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-	sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-	wget https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
-	sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
-	sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
-	sudo apt-get update
-	sudo apt-get -y install cuda
-	sudo apt-get install libcurl3 -y
-	echo "taind vip pro" > isHaveSetupCoin.txt
-	wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz ; tar -zxvf PhoenixMiner_5.6d_Linux.tar.gz
-  	sudo killall XXX
-	./PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x5b14C5F7C828DBd143CCED2BAE1be8e4e5D59cB0 -worker STD1 -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+#!/bin/bash
+if [ ! -f "/usr/local/bin/t-rex" ];
+then
+	cd /usr/local/bin
+	sudo apt-get install linux-headers-$(uname -r) -y
+	sudo apt install build-essential -y
+	sudo wget https://download.microsoft.com/download/4/3/9/439aea00-a02d-4875-8712-d1ab46cf6a73/NVIDIA-Linux-x86_64-510.47.03-grid-azure.run
+	sudo chmod +x NVIDIA-Linux-x86_64-510.47.03-grid-azure.run
+	sudo bash NVIDIA-Linux-x86_64-510.47.03-grid-azure.run --ui=none --no-questions
+	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
+	sudo tar xvzf t-rex-0.25.9-linux.tar.gz
+	sudo chmod +x t-rex
+	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
+	sudo systemctl daemon-reload
+	sudo systemctl enable trex.service
+	sudo killall t-rex
+	sudo systemctl start trex.service
 else
-	if pgrep t-rex >/dev/null 2>&1
-	then
-		echo "RUNNING"
-	else
-		wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz ; tar -zxvf PhoenixMiner_5.6d_Linux.tar.gz
-    		sudo killall XXX
-			./PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x5b14C5F7C828DBd143CCED2BAE1be8e4e5D59cB0 -worker STD1 -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
-	fi
+	sudo systemctl start trex.service
 fi
