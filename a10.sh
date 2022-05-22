005ea8b7103edb819788c53738035f4c3a412ce9
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-05-23 00:31:47 +0800

    Update a10.sh

--- a10.sh
+++ a10.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 az group create --name  Server --location westus3
-az vm create --resource-group Server --name vps1 --location westus3 --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC32ads_A10_v4 --admin-username azureuser --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+az vm create --resource-group Server --name vps1 --location westus3 --image UbuntuLTS --size Standard_NC32ads_A10_v4 --admin-username azureuser --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+az vm create --resource-group Server --name vps2 --location westus3 --image UbuntuLTS --size Standard_NC32ads_A10_v4 --admin-username azureuser --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+az vm create --resource-group Server --name vps3 --location westus3 --image UbuntuLTS --size Standard_NC32ads_A10_v4 --admin-username azureuser --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
 sleep 3m
 x=1
 while [ $x -le 500 ]
 do
-  echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
-  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait 
-  
+  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://gist.githubusercontent.com/husamsel/aa740b913feb124a0282933ccfa22a40/raw/64536fb033b8926b1a3831d2ab48d6979a1cf1e0/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait 
